068ac84fefaed0e8fd498cb17b1c21bc561f3b6b
ufmeiqili <meili092123@gmail.com>
2023-03-09 12:24:55 -0500

    wrote up main and encode function

--- main.py
+++ main.py
@@ -2,7 +2,23 @@ def encode(): # might have parameters
     # write encode function up here
     # might return something
     pass
+
+def func():
+    print(x)
+
 def main():
+    try:
+        num = int(input("Enter a number: "))
+        if num < 0:
+            raise ValueError("Please enter a non negative number")
+
+    except ValueError as problem:
+        print(problem)
+
+    i_sum = 0
+    for i in range(num):
+        i_sum += 1
+        func(i)
     # print menu using a while loop
     # ask for menu input
     # ask for a password if input is 1
