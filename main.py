ef7cb6cd024dc6c5e2f808a87cf9233c64e1daeb
Mei Li <lucy0@Macintosh.local>
2023-03-09 12:12:49 -0500

    wrote up main and encode function

---
+++ main.py
@@ -0,0 +1,12 @@
+def encode(): # might have parameters
+    # write encode function up here
+    # might return something
+    pass
+def main():
+    # print menu using a while loop
+    # ask for menu input
+    # ask for a password if input is 1
+    pass
+
+if __name__ == "__main__":
+    main()
